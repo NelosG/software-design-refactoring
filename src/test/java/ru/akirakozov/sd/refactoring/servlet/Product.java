d0ae5738b12414e138577f1d82ed548f2450426c
NelosG <gleb.pushkarev@gmail.com>
2022-11-07 05:43:23 +0300

    Added GetProductTest

---
+++ src/test/java/ru/akirakozov/sd/refactoring/servlet/Product.java
@@ -0,0 +1,28 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import java.util.Objects;
+
+class Product {
+    final String name;
+    final int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof Product) {
+            Product product = (Product) o;
+            return price == product.price && name.equals(product.name);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
